5b640fa490be217f1203f58a80324f5d4b1f1ea6
Luis Sousa <lv-ss@hotmail.com>
2020-12-06 02:29:26 -0300

    finalizando resolução

--- api.js
+++ api.js
@@ -1,5 +1,16 @@
 const api = (config, callback) => {
+  const req = new XMLHttpRequest();
 
+  req.addEventListener('readystatechange', function(){
+    if (this.readyState === 4){
+      const response = JSON.parse(this.response);
+
+      callback(response);  
+    }
+  })
+
+  req.open(config.method, config.url);
+  req.send(config.data)
 }
 
 export default api
--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 import api from './api.js'
 
+const $productList = document.querySelector('.product-list');
 
-//Neste exercício você deve imprimir na UL ".product-list" produtos seguindo o layout no README do gitbub: https://i.imgur.com/EbVlWpX.png
+const config = { 
+  method: 'GET',
+  url: 'products.json',
+  data: null
+}
+
+const limitName = (name, tam) => {
+  const limitedName = [...name].reduce((acc, letter, index) => {
+    if (index === tam) return acc + '...';
+    if (index > tam) return acc;
+    return acc + letter;
+  }, '')
+
+  return limitedName;
+}
+
+const render = (response) => {
+  console.log(response.item)
+  response.item.forEach((item) => {
+    const limitedName = limitName(item.name, 50)
+    $productList.innerHTML +=
+      `<li>
+        <div class='item-box'>
+          <img src='${item.image}' alt='produto'>
+          <div class='content'>
+            <p class='title'>${limitedName}</p>
+            <div class='content-info'>
+              <p class='qtd'>Qtd. ${item.quantity}</p>
+              <span class='price'>${item.bestPriceFormated}</span>
+          </div>
+          </div>
+        </div>
+      </li>`
+  
+  })
+}
+
+
+api(config, render)
+//Neste exercício você deve imprimir na UL ".product-list" produtos seguindo o layout no README do github: https://i.imgur.com/EbVlWpX.png
 //Deve incluir a soma dos produtos
 //O botão finalizar compra deve ter o href de "/checkout"
